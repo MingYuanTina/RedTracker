afc6feb95d4deea8a5871d07c8812428f431f0c8
Tina Li <bjteana@gmail.com>
2016-11-27 10:49:56 -0500

    Add files via upload

---
+++ RedTracker/src/SurgiNeer/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SurgiNeer.Data;
+using SurgiNeer.Models;
+
+namespace SurgiNeer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Applies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Apply.ToListAsync());
+        }
+
+        // GET: Applies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apply = await _context.Apply.SingleOrDefaultAsync(m => m.ID == id);
+            if (apply == null)
+            {
+                return NotFound();
+            }
+
+            return View(apply);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+    }
+}
---
+++ RedTracker/src/SurgiNeer/Models/Data/Migrations/20161127035925_StartDate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SurgiNeer.Data.Migrations
+{
+    public partial class StartDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Apply");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Apply",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Apply");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Apply",
+                nullable: true);
+        }
+    }
+}
